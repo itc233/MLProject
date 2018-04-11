93322e1f79ebf96ccde36fefe99ba2f649ce657e
itc233 <itc233@nyu.edu>
2018-04-11 00:34:43 +0000

    first version

--- sample.py
+++ sample.py
@@ -19,7 +19,7 @@ Xtr, ytr, Xvald, yvald = sample_data(X, y, port_tr, port_vald)
 GAtrain = np.column_stack([Xtr, ytr])
 GAvalid = np.column_stack([Xvald, yvald])
 
-(sample_result, sample_genes, sample_scores) = GA(GAtrain, GAvalid, linear_model.LinearRegression(), feval, iter=2, r_sample=0.9, r_crossover=0.5, r_vary=0.05, verbose = True).select(axis = 1)
+(sample_result, sample_genes, sample_scores) = GA(GAtrain, GAvalid, linear_model.LinearRegression(), feval, iter=200, r_sample=0.9, r_crossover=0.5, r_vary=0.05, verbose = True).select(axis = 1)
 
 print("sample_result:\n", sample_result)
 print("sample_genes:\n", sample_genes)
