7b31b953a5fbe31ae0b34876fc6a9135db2c3222
itc233 <itc233@nyu.edu>
2018-04-23 00:47:28 -0400

    test

--- genetics.py
+++ genetics.py
@@ -143,10 +143,7 @@ class GA(object):
                     ch2[j] = ch1[j]
                 do_mut = random.uniform(0, 1)
                 if(do_mut < self.r_vary):
-                    if(ch1[j]):
-                        ch1[j] = False
-                    else :
-                        ch1[j] = True
+                    ch1[j] = !ch1[j]
                 do_mut = random.uniform(0, 1)
                 if(do_mut < self.r_vary):
                     if(ch2[j]):
