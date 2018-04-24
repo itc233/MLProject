bafb508ca523262c72ace1f51929409e59dacc8c
itc233 <itc233@nyu.edu>
2018-04-24 18:13:32 -0400

    test validation

--- sample.py
+++ sample.py
@@ -28,6 +28,9 @@ Xtr, ytr, Xvald, yvald = sample_data(Xtr, ytr, port_tr, port_vald)
 GAtrain = np.column_stack([Xtr, ytr])
 GAvalid = np.column_stack([Xvald, yvald])#([Xts.astype(float), yts.astype(float)])
 
+np.save('trainDataForGA', GAtrain)
+np.save('validDateForGA', GAvalid)
+
 (sample_result, sample_genes, sample_scores) = GA(GAtrain, GAvalid, linear_model.LinearRegression(), feval, iter=50, r_sample=0.5, r_crossover=0.5, r_vary=0.0, r_keep_best = 0.01, popsize = 300, verbose = True).select()
 
 print("sample_result:\n", sample_result)
@@ -36,5 +39,3 @@ print("sample_scores:\n", sample_scores)
 
 np.save('trained_genes', sample_genes)
 np.save('hist_scores', sample_scores)
-np.save('trainDataForGA', GAtrain)
-np.save('validDateForGA', GAvalid)
