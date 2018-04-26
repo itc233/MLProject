1c43f818c52fa3f0fe535720107a8acde9fbc826
itc233 <itc233@nyu.edu>
2018-04-26 18:54:24 -0400

    crossvalidation

--- mysrc.py
+++ mysrc.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
-from sklearn import linear_model, preprocessing, metrics
+from sklearn import linear_model, preprocessing, metrics, model_selection
 #from parse import read_data
 from genetics import GA
+
 
 def generate_idx(port_tr, port_vald, data_len):
 	tr_len = np.int(port_tr*data_len)
@@ -15,9 +16,23 @@ def generate_idx(port_tr, port_vald, data_len):
 	np.save("vald_idx.txt", vald_idx)
 	return tr_idx, vald_idx
 
-def feval(yhat, y):
-	fit = np.sqrt(np.mean((y-yhat)**2))
-	return fit
+#def feval(yhat, y):
+#	fit = np.sqrt(np.mean((y-yhat)**2))
+#	return fit
+
+def feval(estimator, X, gene, nfold):
+	kf = model_selection.KFold(n_splits=nfold,shuffle=True)
+	RMSE = np.zeros(nfold)
+	for isplit, Ind in enumerate(kf.split(X)):
+		Itr, Its = Ind 
+		xtr = X[Itr, gene]
+		ytr = X[Itr, -1]
+		xts = X[Its, gene]
+		yts = X[Its, -1]
+		model = estimator.fit(xtr, ytr)
+		yhat = model.predict(xts)
+		RMSE[isplit] = np.sqrt(np.mean(yts-yhat)**2)
+	return np.mean(RMSE)
 
 def sample_data(X, y, port_tr, port_vald):
 	tr_idx, val_idx = generate_idx(port_tr, port_vald, len(y))
