575459957beadcf2700ea433da219549f9fa2535
itc233 <itc233@nyu.edu>
2018-05-11 01:06:56 -0400

    test

--- sample.py
+++ sample.py
@@ -17,19 +17,24 @@ tsy_path = 'data/test_y.csv'
 
 Xtr, ytr = readDataTrain(tr_path)
 Xts, yts = readDataTest(tsx_path, tsy_path)
-X = np.concatenate((Xtr, Xts), axis = 0)
+#X = np.concatenate((Xtr, Xts), axis = 0)
 #X = preprocessing.scale(X)
-Xtr = X[:len(ytr), :]
+#Xtr = X[:len(ytr), :]
+#Xts = X[len(ytr):, :]
+#np.save('data/train', np.column_stack([Xtr, ytr]))
+#np.save('data/test', np.column_stack([Xts, yts]))
+
+Xytr = np.column_stack([Xtr, ytr])
+Xytr = rejectOutlier(Xytr, 3)
+Xtr = Xytr[:, :-1]
+X = np.concatenate((Xtr, Xts), axis = 0)
+X = preprocessing.scale(X)
+Xytr[:,:-1] = X[:len(ytr), :]
 Xts = X[len(ytr):, :]
-np.save('data/train', np.column_stack([Xtr, ytr]))
-np.save('data/test', np.column_stack([Xts, yts]))
+np.save('trainDataForGA', Xytr)
+np.save('testDataForGA', np.column_stack([Xts, yts]))
 
-GAtrain = np.column_stack([Xtr, ytr])
-GAtrain = rejectOutlier(GAtrain, 3)
-GAtrain[:,:-1] = preprocessing.scale(GAtrain[:,:-1])
-np.save('trainDataForGA', GAtrain)
-
-(sample_result, sample_genes, sample_scores) = GA(GAtrain, linear_model.Lasso(alpha = 0.01), feval, iter=300, r_sample=0.5, r_crossover=0.5, r_vary=0.0, r_keep_best = 0.1, popsize = 300, pTrain = 0.8, nfold = 5, verbose = True).select()
+(sample_result, sample_genes, sample_scores) = GA(Xytr, linear_model.Lasso(alpha = 0.01), feval, iter=300, r_sample=0.5, r_crossover=0.5, r_vary=0.0, r_keep_best = 0.1, popsize = 300, pTrain = 0.8, nfold = 5, verbose = True).select()
 
 print("sample_result:\n", sample_result)
 print("sample_genes:\n", sample_genes)
