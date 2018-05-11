6c33cb0b8bed354835d03d6893613f86b10d9180
itc233 <itc233@nyu.edu>
2018-05-11 04:23:22 +0000

    test

--- sample.py
+++ sample.py
@@ -18,17 +18,18 @@ tsy_path = 'data/test_y.csv'
 Xtr, ytr = readDataTrain(tr_path)
 Xts, yts = readDataTest(tsx_path, tsy_path)
 X = np.concatenate((Xtr, Xts), axis = 0)
-X = preprocessing.scale(X)
+#X = preprocessing.scale(X)
 Xtr = X[:len(ytr), :]
 Xts = X[len(ytr):, :]
 np.save('data/train', np.column_stack([Xtr, ytr]))
 np.save('data/test', np.column_stack([Xts, yts]))
 
 GAtrain = np.column_stack([Xtr, ytr])
-GAtrain = rejectOutlier(GAtrain, 2)
+GAtrain = rejectOutlier(GAtrain, 3)
+GAtrain[:,:-1] = preprocessing.scale(GAtrain[:,:-1])
 np.save('trainDataForGA', GAtrain)
 
-(sample_result, sample_genes, sample_scores) = GA(GAtrain, linear_model.LinearRegression(), feval, iter=300, r_sample=0.5, r_crossover=0.5, r_vary=0.0, r_keep_best = 0.1, popsize = 200, pTrain = 0.8, nfold = 5, verbose = True).select()
+(sample_result, sample_genes, sample_scores) = GA(GAtrain, linear_model.Lasso(alpha = 0.01), feval, iter=300, r_sample=0.5, r_crossover=0.5, r_vary=0.0, r_keep_best = 0.1, popsize = 300, pTrain = 0.8, nfold = 5, verbose = True).select()
 
 print("sample_result:\n", sample_result)
 print("sample_genes:\n", sample_genes)
